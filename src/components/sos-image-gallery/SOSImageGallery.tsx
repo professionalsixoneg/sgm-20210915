9d31af73dab8ee94aa8c41ad0fd3d20176657374
Sahar S1 <sahar.s00001@gmail.com>
2021-09-16 17:23:57 +0530

    Back to Lobby Button

--- src/components/sos-image-gallery/SOSImageGallery.tsx
+++ src/components/sos-image-gallery/SOSImageGallery.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ReactImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Gallery from '../../models/gallery';
@@ -6,16 +7,23 @@ import './SOSImageGallery.css';
 
 const SOSImageGallery: React.FC<{ gallery: Gallery }> = ({ gallery }) => {
     return (
-        <div className="sos-gallery-container">
-            <ReactImageGallery
-                items={gallery.images.map<ReactImageGalleryItem>(img => {
-                    return {
-                        original: img,
-                        thumbnail: img,
-                    }
-                })}
-            />
-        </div>
+        <>
+            <Link to="/">
+                <div className="sos-back-to-lobby-button btn btn-light">
+                    Back to Lobby
+                </div>
+            </Link>
+            <div className="sos-gallery-container">
+                <ReactImageGallery
+                    items={gallery.images.map<ReactImageGalleryItem>(img => {
+                        return {
+                            original: img,
+                            thumbnail: img,
+                        }
+                    })}
+                />
+            </div>
+        </>
     );
 }
 
