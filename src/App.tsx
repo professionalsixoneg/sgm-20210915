e70f63d4e101216c427896d83523c929b3fb04d1
Sahar S1 <sahar.s00001@gmail.com>
2021-09-16 14:20:29 +0530

    Add ImageGallery Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import SOSImageGallery from './components/sos-image-gallery/SOSImageGallery';
+import GALLERIES from './data/galleries';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <SOSImageGallery gallery={GALLERIES[1]} />
+    </div>
   );
 }
 
---
+++ src/components/sos-image-gallery/SOSImageGallery.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import ReactImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Gallery from '../../models/gallery';
+import './SOSImageGallery.css';
+
+const SOSImageGallery: React.FC<{ gallery: Gallery }> = ({ gallery }) => {
+    return (
+        <div className="sos-gallery-container">
+            <ReactImageGallery
+                items={gallery.images.map<ReactImageGalleryItem>(img => {
+                    return {
+                        original: img,
+                        thumbnail: img,
+                    }
+                })}
+            />
+        </div>
+    );
+}
+
+export default SOSImageGallery;
