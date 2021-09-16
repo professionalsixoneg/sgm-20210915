10df3cee218d61b260a965583422b68019ac00da
Sahar S1 <sahar.s00001@gmail.com>
2021-09-16 16:22:39 +0530

    Show Gallery Banners in Listing

--- src/components/sos-galleries-grid/SOSGalleriesListing.tsx
+++ src/components/sos-galleries-grid/SOSGalleriesListing.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Gallery from '../../models/gallery';
 import './SOSGalleriesListing.css';
 
 const SOSGalleriesListing: React.FC<{ galleries: Gallery[] }> = ({ galleries }) => {
     return (
-        <ul>
-            {galleries.map(gallery => (
-                <Link to={`/${gallery.path}`}>
-                    <li><h1>{gallery.title}</h1></li>
-                </Link>
-            ))}
-        </ul>
+        <Container className="sos-galleries-listing-container">
+            <Row>
+                {galleries.map(gallery => (
+                    <Col xs="12" md="6" lg="4">
+                        <Link to={`/${gallery.path}`} className="sos-gallery-tile-link">
+                            <div className="sos-gallery-tile-container">
+                                <div className="sos-gallery-tile-banner">
+                                    <img src={gallery.banner} alt={gallery.title} />
+                                </div>
+                                <span className="sos-gallery-tile-title">{gallery.title}</span>
+                            </div>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
 
