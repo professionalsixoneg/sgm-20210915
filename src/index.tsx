3e012a913ad0e654dbef665e7278395efcc6b098
Sahar S1 <sahar.s00001@gmail.com>
2021-09-19 17:41:54 +0530

    Overrite default data from AppConfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GALLERIES from './data/galleries';
+import CONFIG from './data/config';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+(async function () {
+  const configResponse = await fetch("https://cdn.jsdelivr.net/gh/professionalsixoneg/sgall-assets-sorting@main/AppConfig/AppConfig.json");
+
+  const configJSON = await configResponse.json();
+
+  Object.assign(GALLERIES, configJSON.Galleries);
+  Object.assign(CONFIG, configJSON.Config);
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})()
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
